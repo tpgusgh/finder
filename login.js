bec5f65debde17213ffc2795fa55e9d9b43c879e
hyunho <kimhyunhi1@gmail.com>
2024-07-14 22:35:45 +0900

    1

--- login.js
+++ login.js
@@ -3,7 +3,7 @@ function login() {
     const password = document.getElementById('password').value;
     if (id === 'kimhyunhi' && password === 'bssm1234') {
         alert('김현호님 환영합니다.')
-        location.href = '텍스트 입력받기.html';
+        location.href = 'textfind.html';
     } else {
         alert('아이디 또는 비밀번호가 잘못되었습니다.');
     }
